1fd2a18ca86fe1f1d977acd75ae3b099d1e61bf5
erico <ecocaval@hotmail.com>
2023-01-24 19:21:59 -0300

    Changing useEffect from HomePage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -110,7 +110,9 @@ export default function HomePage() {
         }
     }
 
-    useEffect(() => getUserInfo, [])
+    useEffect(() => {
+        getUserInfo()
+    }, [])
 
     useEffect(function treatAnimation() {
         userTransactions.forEach((_, index) => {
